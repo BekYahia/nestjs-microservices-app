7afb1af85fec14d769bb50dc62a146f0f71a8e22
= <albakrieee@gmail.com>
2023-09-07 00:16:49 +0000

    add Graphql API gateway

--- apps/gateway/src/gateway.module.ts
+++ apps/gateway/src/gateway.module.ts
@@ -1,10 +1,48 @@
 import { Module } from '@nestjs/common';
-import { GatewayController } from './gateway.controller';
-import { GatewayService } from './gateway.service';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { LoggerModule } from '@app/common';
+import { GraphQLModule } from '@nestjs/graphql';
+import { ApolloGatewayDriver, ApolloGatewayDriverConfig } from '@nestjs/apollo';
+import { IntrospectAndCompose, RemoteGraphQLDataSource } from '@apollo/gateway'
 
 @Module({
-  imports: [],
-  controllers: [GatewayController],
-  providers: [GatewayService],
+  imports: [
+	ConfigModule.forRoot({
+		isGlobal: true,
+	}),
+	LoggerModule,
+	GraphQLModule.forRootAsync<ApolloGatewayDriverConfig>({
+		driver: ApolloGatewayDriver,
+		useFactory: (configService: ConfigService) => ({
+			server: {
+				// context: //attach this func to every req to the downstream service 
+			},
+			gateway: {
+				supergraphSdl: new IntrospectAndCompose({
+					subgraphs: [
+						{
+							name: 'users',
+							url: configService.getOrThrow<string>('USERS_GRAPHQL_URL'),
+						},
+					],
+				}),
+				buildService({ url }) {
+					return new RemoteGraphQLDataSource({
+						url,
+						willSendRequest({ request, context }) { //set the response you get from authContext as header prop
+							request.http.headers.set(
+								'user',
+								context.user ? JSON.stringify(context.user) : null
+							)
+						}
+					});
+				},
+			},
+		}),
+		inject: [ConfigService],
+	}),
+  ],
+  controllers: [],
+  providers: [],
 })
 export class GatewayModule {}
--- apps/gateway/src/main.ts
+++ apps/gateway/src/main.ts
@@ -1,8 +1,13 @@
 import { NestFactory } from '@nestjs/core';
 import { GatewayModule } from './gateway.module';
+import { ConfigService } from '@nestjs/config';
+import { Logger } from 'nestjs-pino';
 
 async function bootstrap() {
   const app = await NestFactory.create(GatewayModule);
-  await app.listen(3000);
+  const config = app.get(ConfigService)
+
+  app.useLogger(app.get(Logger))
+  await app.listen(config.getOrThrow<string>('PORT'));
 }
 bootstrap();
